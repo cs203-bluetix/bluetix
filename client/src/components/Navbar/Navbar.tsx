45123979331483cac519e87b7472ec771dbdfdc5
tkyj <82047680+tkyj@users.noreply.github.com>
2023-09-03 22:34:10 +0800

    Added navbar css

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,26 +1,70 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "utils/Button";
+import LoginModal from "utils/LoginModal";
+
 
 function Navbar() {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const toggleModal = () => setIsModalOpen(!isModalOpen);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  
   return (
-    <nav className="fixed w-full h-16 px-3 z-50 bg-transparent text-[#3D7BBD] -z-1">
-      <div className="flex flex-wrap justify-between items-center h-full w-full px-20 2xl:px-16">
-        <div>  <Link href='/' className="text-4xl font-bold ml-10"> BlueTix</Link>
-        </div>
-        <div className="w-1/3 flex justify-between items-center">
-          <Link href='/'> About Us </Link>
-          <Link href='/'> Events </Link>
-          <Link href='/'> Venues </Link>
-          <Link href='/'> FAQ </Link>
-          <button className="bg-gradient-to-br from-cyan-200 to-blue-600 opacity-80 px-4 py-1 rounded-lg">
-            Log In
-          </button>
-        </div>
+    <>
+    <div id="root" className="mx-auto max-w-screen-lg px-3 absolute inset-x-0 py-4 bg-opacity-70 backdrop-blur-lg">
+      <div className="flex flex-wrap justify-between items-center">
+        <Link href="/" className="text-3xl font-bold">
+          BlueTix
+        </Link>
+        <nav>
+          <ul className="navbar flex items-center font-medium">
+            <li>
+              <Link href="/aboutus">About</Link>
+            </li>
+            <li>
+              <Link href="/events">Events</Link>
+            </li>
+            <li>
+              <Link href="/venues">Venues</Link>
+            </li>
+            <li>
+              <Link href="/faq">FAQ</Link>
+            </li>
+            <li>
+              <Button onClick={toggleModal}>
+                {/* <Link href="/login"> */}
+                  Log In
+                {/* </Link> */}
+              </Button>
+            </li>
+          </ul>
+        </nav>
 
+        <style jsx>
+          {`
+            .navbar :global(li:not(:first-child)) {
+              margin-top: 0;
+            }
 
+            .navbar :global(li:not(:last-child)) {
+              margin-right: 1.25rem;
+            }
+
+          `}
+        </style>
       </div>
-
-    </nav>
+    </div>
+    
+    <LoginModal isOpen={isModalOpen} onClose={closeModal} />
+    </>
   );
 }
 
