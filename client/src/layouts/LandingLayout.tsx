c911412780f9ec272a30c6d1dae32b20acd89e87
adversesg <faviancheong22@gmail.com>
2023-09-05 08:33:38 +0800

    Landing Page

--- client/src/layouts/LandingLayout.tsx
+++ client/src/layouts/LandingLayout.tsx
@@ -2,6 +2,9 @@ import Footer from "components/Footer/Footer";
 import Navbar from "components/Navbar/Navbar";
 import Head from "next/head";
 import React from "react";
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function LandingLayout({ children }: { children: JSX.Element }) {
   return (
@@ -12,7 +15,9 @@ export default function LandingLayout({ children }: { children: JSX.Element }) {
         <link rel="icon" href="/favicon.ico" />
       </Head> 
       <Navbar />
-      <main>{children}</main>
+      <main className={inter.className}>
+        {children}
+      </main>
       <Footer />
     </>
   );
