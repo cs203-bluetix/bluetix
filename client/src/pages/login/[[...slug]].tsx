c13d1c66e7b87d2eee981e9f5f1c61602a26b15e
tkyj <82047680+tkyj@users.noreply.github.com>
2023-09-26 09:21:52 +0800

    Fix landing

--- client/src/pages/login/[[...slug]].tsx
+++ client/src/pages/login/[[...slug]].tsx
@@ -21,7 +21,7 @@ function Auth() {
           }
         }
         return (
-          <div className="bg-gray-200">
+          <div className="">
           <Section>
           <div className="flex h-[calc(100vh-68px)] w-full justify-center">
             <div className="mx-auto mb-8 mt-8 flex w-full max-w-5xl flex-col items-center  px-3 pt-4 sm:px-6 xl:max-w-6xl xl:px-8">
