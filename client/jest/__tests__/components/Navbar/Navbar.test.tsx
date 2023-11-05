415b6d6436ca88e532caa92373d133d3af0d8e6c
adversesg <faviancheong22@gmail.com>
2023-11-05 21:39:47 +0800

    Stashing

--- client/jest/__tests__/components/Navbar/Navbar.test.tsx
+++ client/jest/__tests__/components/Navbar/Navbar.test.tsx
@@ -1,7 +1,28 @@
 import { render, screen } from "@testing-library/react";
 import Navbar from "../../../../src/components/Navbar/Navbar";
+import "@testing-library/jest-dom";
 
 describe("Navbar", () => {
-  it("should display Navbar correctly", () => {});
-  it("clicking links should redirect to correct pages", () => {});
+  it("should display Navbar correctly", () => {
+    render(<Navbar user={null} />);
+    const about = screen.getByText("About"); 
+    expect(about).toBeInTheDocument();
+
+    const events = screen.getByText("Events"); 
+    expect(events).toBeInTheDocument();
+
+    const venues = screen.getByText("Venues");
+    expect(venues).toBeInTheDocument();
+
+    const FAQ = screen.getByText("FAQ");
+    expect(FAQ).toBeInTheDocument();
+
+    screen.debug(undefined, Infinity); 
+
+  });
+
+  it("clicking links should redirect to correct pages", () => {
+    // Write your test logic for link redirection here
+    
+  });
 });
